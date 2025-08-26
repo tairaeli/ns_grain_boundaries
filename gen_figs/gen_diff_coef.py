ca60f888fad62f56e2551c9ecd3fa1aa3392e8e7
Elias Taira <tairaeli@DESKTOP-8QI063P.localdomain>
2025-08-26 08:28:25 -0500

    added visualizations

--- gen_figs/gen_diff_coef.py
+++ gen_figs/gen_diff_coef.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 
-nskip = 209
+nskip = 213
 
 df = pd.read_csv("../grain_bound_sims/log.lammps", skiprows=nskip,
                  delim_whitespace=True)
@@ -14,11 +14,11 @@ msd = df["v_msd_fm"].to_numpy()
 plt.plot(steps, msd)
 plt.xlabel("Steps")
 plt.ylabel("MSD (fm)")
-plt.savefig("msd_plot.png")
+plt.savefig("./lj_run_8_20_msd_plot.png")
 
 # generating fit
 model = LinearRegression()
-model.fit(steps.T,msd)
+model.fit(steps.reshape(1, -1),msd)
 
 print(model.coef_, model.intercept_)
 
